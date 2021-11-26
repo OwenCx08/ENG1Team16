ec2dce4a71f3824a22bbb4257444c9ec868e42aa
AlanYJZ <94684109+AlanYJZ@users.noreply.github.com>
2021-11-26 10:54:03 +0000

    Add files via upload

---
+++ College.java
@@ -0,0 +1,5 @@
+public class College extends GameObject{
+    public College(Board Board, String name, int[] pos, int points, int plunder){
+        super(Board, name, pos, points, plunder);
+    }
+}
