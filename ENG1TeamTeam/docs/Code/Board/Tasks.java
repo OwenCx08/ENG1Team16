3775e727e57fa2f6d5bc2d12fa57bdb5840d225a
AlanYJZ <94684109+AlanYJZ@users.noreply.github.com>
2022-01-16 23:46:02 +0000

    Update Tasks.java

--- ENG1TeamTeam/docs/Code/Board/Tasks.java
+++ ENG1TeamTeam/docs/Code/Board/Tasks.java
@@ -1,5 +1,11 @@
 public class Tasks{
     public void Tasks(){
+        #First task--MOVE!
         
+        #Second task--destory an EnemyShip
+            
+        #Third task--destory an EnemyCollege
+          
+        #Last task--destoryanother college(a stranger one?)
     }
 }
