d87cd156ad31f238218d2faefb6a35ee93e8672e
OwenCYork <94682750+OwenCYork@users.noreply.github.com>
2022-01-21 13:03:47 +0000

    Update Map.java

--- ENG1TeamTeam/docs/TeamTeamGame/core/src/com/mygdx/game/Board/Map.java
+++ ENG1TeamTeam/docs/TeamTeamGame/core/src/com/mygdx/game/Board/Map.java
@@ -4,9 +4,14 @@ public class Map{
         int[][] mapGrid
         // 0 is empty tile
         // 1 is a land tile
+        // 2 is friendly college
+        // 3 is friendly ship
+        // 4+ is an enemy entity
 
     }
+    //Coordinates are stored in the format (y,x)
+
     public int getAt(int x, int y){
-        
+        return mapGrid[y][x];
     }
 }
