ed0e27ffc14ebfdd9bb7ba85a8240b984003bff8
OwenCYork <94682750+OwenCYork@users.noreply.github.com>
2022-01-21 12:38:17 +0000

    Update Map.java

--- ENG1TeamTeam/docs/TeamTeamGame/core/src/com/mygdx/game/Board/Map.java
+++ ENG1TeamTeam/docs/TeamTeamGame/core/src/com/mygdx/game/Board/Map.java
@@ -1,6 +1,12 @@
 package com.mygdx.game.Board;
 public class Map{
     public void Map(){
+        int[][] mapGrid
+        // 0 is empty tile
+        // 1 is a land tile
+
+    }
+    public int getAt(int x, int y){
         
     }
 }
