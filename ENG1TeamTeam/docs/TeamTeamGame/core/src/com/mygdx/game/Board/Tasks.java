e94fe9a9d6f2467713bf13a51c139c2160fe033e
AlanYJZ <94684109+AlanYJZ@users.noreply.github.com>
2022-01-24 00:49:16 +0000

    Update Tasks.java

--- ENG1TeamTeam/docs/TeamTeamGame/core/src/com/mygdx/game/Board/Tasks.java
+++ ENG1TeamTeam/docs/TeamTeamGame/core/src/com/mygdx/game/Board/Tasks.java
@@ -52,10 +52,29 @@ public class Tasks implements ApplicationListener {
             batch.begin();
             font.draw(batch, "Time to destory som ships,try to click that enemy ship to attack it.", 200, 200);
             batch.end();
+            //if ship hp == 0 then state2
+        
+        }
             
         //Third task--destory the first EnemyCollege
+        while(taskState == 2){
+            Gdx.gl.glClearColor(1, 1, 1, 1);
+            Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         
+            batch.begin();
+            font.draw(batch, "At North-east of the lake, there is a college with great wealth. Conquer that college can help you to upgrade your ship.", 200, 200);
+            batch.end();
+            //if ship college1hp=0 then state3
+        
+        }
         //Last task--destoryanother college(a stranger one?)
+        while(taskState == 3){
+            Gdx.gl.glClearColor(1, 1, 1, 1);
+            Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
         
+            batch.begin();
+            font.draw(batch, "Now, sail to the south-west and conquer the last college.", 200, 200);
+            batch.end();
+            //if ship college2hp=0 then state4
     }
 }
