8511c3bad4fbba2f37ed4544b538c419c5bd624a
OwenCYork <ogc514@york.ac.uk>
2022-02-01 12:00:24 +0000

    JavaDoc for EnemyShip

--- ENG1TeamTeam/docs/TeamTeamGame/core/src/com/mygdx/game/GameObjects/Ships/EnemyShip.java
+++ ENG1TeamTeam/docs/TeamTeamGame/core/src/com/mygdx/game/GameObjects/Ships/EnemyShip.java
@@ -1,19 +1,40 @@
 package com.mygdx.game.GameObjects.Ships;
 import com.mygdx.game.Board.Map;
 public class EnemyShip extends Ship{
+    /**
+     * Stores whether or not the ship is alive
+     */
     private Boolean alive;
+    /**
+     * Stores the ID of the enemy that the ship belongs to
+     */
     protected int enemyID;
+    /**
+     * Stores the current layout of the map
+     */
     private Map gameMap;
-    
+    /**
+     * 
+     * @param id the Unique idendifier for the entity
+     * @param spriteName the file name of the entity's sprite
+     * @param health the entity's hitpoints
+     * @param position the coordinates of the entity
+     * @param range the attack range of the ship
+     * @param moveRange how far the ship can move in a single move
+     * @param enemyID the ID of the enemy that the ship belongs to
+     * @param gameMap the current layout of the map
+     */
     public EnemyShip(int id, float width,float height, String spriteName, int health, int[] position, int range, int moveRange,int enemyID, Map gameMap){
         super(id, width, height, spriteName, health, position,range,moveRange);
         this.gameMap = gameMap;
         this.enemyID = enemyID;
         this.alive=true;
         this.position = position;
-        //System.out.println(this.position);
     }
-    //Searches around a radius of 2 around the ship to see if the player is there
+    /**
+     * Searches around a radius of 2 around the ship to see if the player is there
+     * @return The position of the player if it is in range otherwise returns null
+     */
     public int[] checkForPlayer(){
         int[] data = {-1,-1};
         for(int i=-2;i<3;i++){
@@ -25,24 +46,37 @@ public class EnemyShip extends Ship{
                 }*/
             }
         }
-        //Get player pos
-        //  - if in range return location/head to location
+
         return(null);
     }
-
+/**
+ * Checks if the ship is still alive by checking if its health has reached 0
+ * @return whether or not the ship is alive
+ */
     public Boolean checkIfAlive(){
         if(this.health<=0){
             this.alive=false;
-            //Respawn?
         }
         return(this.alive);
     }
+    /**
+     * gets the name of the Sprite used by the ship
+     * @return the file name of the entity's sprite
+     */
     public String getSpriteName(){
         return this.spriteName;
     }
+    /**
+     * gets the X coordinate of the ship's position
+     * @return the X coordinate of the ship's positione
+     */
     public int getX(){
         return this.position[0];
     }
+        /**
+     * gets the Y coordinate of the ship's position
+     * @return the Y coordinate of the ship's positione
+     */
     public int getY(){
         return this.position[1];
     }
